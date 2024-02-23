6318df1b87cedde679163bc393d29cc6a2a56dc3
Daniel Filemon <danielfilemondev@gmail.com>
2024-02-23 20:45:40 -0300

    update 'ex024'

--- ex024.py
+++ ex024.py
@@ -1,10 +1,10 @@
-cidade = (input('Digite o nome da sua cidade: ')).lower()
+cidade = input('Digite o nome da sua cidade: ').lower().strip()
 
 re = cidade.split()
 
-if re[0] == 'santos':
+if re[0] == 'santo':
 
-    print('Sua cidade começa com Santos. ')
+    print('Sua cidade começa com Santo. ')
 
 else: 
-    print('Sua cidade não começa com Santos. ')
+    print('Sua cidade não começa com Santo. ')
