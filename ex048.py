1432e5fea13f2dafaf384a145bbfd5dd475061e4
Daniel Filemon <danielfilemondev@gmail.com>
2024-02-27 19:18:35 -0300

    Modified: 'ex048', created: 'ex050'

--- ex048.py
+++ ex048.py
@@ -6,23 +6,25 @@ def somar_intervalo():
     tipo = input('Gostaria que fosse somado os números pares ou impares? Pares/Impares ').lower().strip()
 
     s = 0
+    qnt_valores = 0
     if tipo == 'pares':    
         for c in range(nmr_i, nmr_f):
             if c % 2 == 0:
                 if c % 3 == 0:
                     s += c
-        return s
+                    qnt_valores += 1
+        print(f'A soma de todos os {qnt_valores} valores solicitados é {s}. ')
+
     elif tipo == 'impares':
         for c in range(nmr_i, nmr_f):
             if c % 2 != 0:
                 if c % 3 == 0:
                     s += c
-        return s
+                    qnt_valores += 1
+        print(f'A soma de todos os {qnt_valores} valores solicitados é {s}. ')
 
 
 
 print('Olá espero que esteja bem!! Vou realizar uma soma, com somente os números que são múltiplos de três... \nPoderia me informar os números para a contagem? ')
 
-resultado = somar_intervalo()
-
-print(resultado)
+somar_intervalo()
