cd211145df38a34086b4d2727af2d067df3f2f26
Daniel Filemon <danielfilemondev@gmail.com>
2024-02-28 16:50:18 -0300

    modified: 'ex056'

--- ex056.py
+++ ex056.py
@@ -1,6 +1,6 @@
 qnt_pessoas = int(input('Quantas pessoas vão participar dessa pesquisa? '))
 
-
+soma_idade = 0
 media_idade = 0
 contador_media = 0
 mais_velho = 0
@@ -11,14 +11,14 @@ qnt_mulheres = 0
 
 for i in range(0, qnt_pessoas):
     print('-'*5, ' ' , i + 1,'ª PESSOA ', '-'*5)
-    name = input('Nome: ')
+    name = input('Nome: ').strip()
     age = int(input('Idade: '))
     sexo = input('Sexo [M/F]: ').lower().strip()
+    soma_idade += age
+    contador_media += 1
 
     if i == 0:
-        soma_idade = age
-        contador_media = 1
-        
+
         if sexo == 'm':
             nome_homem_velho = name
             mais_velho = age
@@ -26,9 +26,6 @@ for i in range(0, qnt_pessoas):
             if age < 20:
                 qnt_mulheres += 1
     elif i < qnt_pessoas -1:
-        soma_idade += age
-        contador_media += 1
-
         
         if sexo == 'm':
             if age > mais_velho:
@@ -38,10 +35,7 @@ for i in range(0, qnt_pessoas):
             if age < 20:
                 qnt_mulheres += 1
     else:
-        soma_idade += age
-        contador_media += 1
         media_idade = (soma_idade / contador_media)
-
         if sexo == 'm':
             if age > mais_velho:
                 nome_homem_velho = name
@@ -51,8 +45,8 @@ for i in range(0, qnt_pessoas):
                 qnt_mulheres += 1
 
 
-print(f'A média de idade do grupo é de {media_idade}.')
-print(f'O homem mais velho tem {media_idade} anos e se chama {nome_homem_velho}.')
+print(f'A média de idade do grupo é de {media_idade:.2f}.')
+print(f'O homem mais velho tem {mais_velho} anos e se chama {nome_homem_velho}.')
 print(f'Ao todo são {qnt_mulheres} mulheres com menos de 20 anos.')
 
 
