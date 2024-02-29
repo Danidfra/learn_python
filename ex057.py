36ff1a6eff99f9028beaa8cbf51d62dc24811eeb
Daniel Filemon <danielfilemondev@gmail.com>
2024-02-28 22:06:02 -0300

    modified: 'ex057'

--- ex057.py
+++ ex057.py
@@ -4,4 +4,4 @@ print(sexo)
 
 while sexo != 'm' and sexo != 'f':
     sexo = input('Você digitou um sexo que não esta nas opções. Digite o seu sexo: M/F ').lower().strip()
-    print(sexo)
+    print(f'Sexo {sexo} registrado com sucesso.')
