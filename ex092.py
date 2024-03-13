3514d7735cca07b524a0d71878eed715e0a92f6a
Daniel Filemon <danielfilemondev@gmail.com>
2024-03-12 21:15:42 -0300

    modified: 'ex092'

--- ex092.py
+++ ex092.py
@@ -1,6 +1,6 @@
 import datetime;
 
-data_ano = datetime.date.today().year
+data_ano = datetime.datetime.now().year
 
 cp = dict()
 
@@ -10,7 +10,7 @@ cp["ctps"] = int(input('Carteira de Trabalho (0 não tem): '))
 
 if cp["ctps"] != 0:
     cp["contratacao"] = int(input('Ano de contratação: '))
-    cp["salario"] = float(input('Salário: '))
+    cp["salario"] = float(input('Salário: R$'))
     cp["aposentadoria"] = cp["idade"] + (35 - (data_ano - cp["contratacao"]))
 
     print(f'Esta pessoa se chama {cp["nome"]}, tem {cp["idade"]} anos e possui os seguintes dados em sua carteira de trabalho:\n - Nº {cp["ctps"]}\n - Foi contratado no ano de {cp["contratacao"]}\n - Seu salário é de R${cp["salario"]:.2f}\n - Irá se aposentar com {cp["aposentadoria"]} anos')
