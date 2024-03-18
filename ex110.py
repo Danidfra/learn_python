32908ec15d333cf936cc6a2dabc82e9a8d0a3e79
Daniel Filemon <danielfilemondev@gmail.com>
2024-03-17 21:04:43 -0300

    created: 'ex110'

---
+++ ex110.py
@@ -0,0 +1,7 @@
+from func import moeda
+
+p = float(input('Digite o preço: R$'))
+pa = float(input('Digite a porcentagem que será acrescentada: '))
+pr = float(input('Digite a porcentagem que será decrescida: '))
+
+moeda.resumo(p, pa, pr)
