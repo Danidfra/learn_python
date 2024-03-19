b705948f1194d630619c67b4254677f5c2a2b0ed
Daniel Filemon <danielfilemondev@gmail.com>
2024-03-18 22:58:01 -0300

    modifieS

---
+++ mundos/ex104.py
@@ -0,0 +1,12 @@
+def leiaInt(f, r=0):
+    # nmr = input(f)
+    # while not nmr.isnumeric():
+    #     print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
+    #     nmr = input(f)
+    # if nmr.isnumeric():
+    
+    print(f'O valor inteiro digitado foi {f} e o real foi {r:.2f}')
+
+
+# n = leiaInt('Digite um número inteiro: ')
+# print(f'Você acabou de digitar o número {f}.')
