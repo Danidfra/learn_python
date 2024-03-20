d5e7e08860674e8f569cdc0d18d3f869d0a28d59
Daniel Filemon <danielfilemondev@gmail.com>
2024-03-19 22:30:47 -0300

    created: class Tabela para usos futuros

--- func/title/__init__.py
+++ func/title/__init__.py
@@ -1,9 +1,27 @@
-def formaTitulo(tex, tam, tab=False):
-    print('-'*tam)
-    print(tex.center(tam))
-    print('-'*tam)
+# def formaTitulo(tex, tam, item='pessoa', tab=False):
+#     print('-'*tam)
+#     print(tex.center(tam))
+#     print('-'*tam)
 
-    if tab:
-        print('1 - Ver pessoas cadastradas')
-        print('2 - Cadastrar nova Pessoa')
-        print('0 - Sair do Sistema')
+#     if tab:
+#         print(f'1 - Ver {item}s cadastradas')
+#         print(f'2 - Cadastrar {item}')
+#         print('0 - Sair do Sistema')
+
+class Tabela():
+
+    def __init__(self, text, tam, item):
+        self.text = text
+        self.tam = tam
+        self.item = item
+
+        
+    def montarTabela(self):    
+        print('-'*self.tam)
+        print((self.text).center(self.tam))
+        print('-'*self.tam)
+
+        for c,i in enumerate(self.item, start=1):
+                print(f'{c} - {i}.')
+
+        print('-'*self.tam)
